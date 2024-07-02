04090c4b7b4c910f626e61f8682416dcaee78bc8
valescos <valescos1986@gmail.com>
2024-07-02 17:50:19 +0700

    Add twMerge + clsx utility

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 function App() {
-  return <></>;
+  const handleTogleDM = () => {
+    document.querySelector("html")?.classList.contains("dark")
+      ? document.querySelector("html")?.classList.remove("dark")
+      : document.querySelector("html")?.classList.add("dark");
+  };
+
+  return (
+    <>
+      <button onClick={handleTogleDM}>Toggle DarkMode</button>
+    </>
+  );
 }
 
 export default App;
