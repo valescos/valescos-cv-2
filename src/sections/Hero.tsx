458e385173793f80c62b85759f9e1bd151b8966b
valescos <valescos1986@gmail.com>
2024-07-03 16:05:36 +0700

    Aded info for Hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,14 +1,39 @@
 import { useTailwindConst } from "../hooks/useTailwindConst";
 import { cn } from "../utilities/cn";
 
+//temp imports
+import avatar from "../assets/img/avatar_sm.png";
+
 export default function Hero() {
   const { RESPONSIVE_WRAPPER } = useTailwindConst();
 
   return (
-    <section className={cn("flex justify-between py-4", RESPONSIVE_WRAPPER)}>
-      <p>Фото!</p>
-      <p>Косарев ВЛНТН</p>
-      <p>Контакты</p>
+    <section
+      className={cn(
+        "flex items-center justify-center gap-8 py-4",
+        RESPONSIVE_WRAPPER,
+      )}
+    >
+      <div className="overflow-hidden rounded-3xl bg-[url('./assets/img/day-theme-bg.jpg')] bg-cover transition-all dark:bg-[url('./assets/img/nigth-theme-bg.jpg')]">
+        <img
+          src={avatar}
+          alt="Аватар автора сайта"
+          className="dark:saturate-[0.65]"
+        />
+      </div>
+      <div>
+        <h1>Валентин Косарев</h1>
+        <p>Фронтенд-разработчик</p>
+        <p>
+          <a href="https://github.com/valescos">valescos</a>
+        </p>
+        <p>
+          <a href="mailto:valescos1986@gmail.com">valescos1986@gmail.com</a>
+        </p>
+        <p>
+          <a href="https://t.me/Valescos">t.me/Valescos</a>
+        </p>
+      </div>
     </section>
   );
 }
